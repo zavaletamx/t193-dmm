1ca49d93e92305d6f8dde15da06b96b574489ab3
ZavaletaMX <raul.zavaletazea@gmail.com>
2021-01-26 14:25:07 -0600

    Instalación de React Navigation y uso de StackNavigator

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Para utilizar la navegación de React necesitamos
+ * 1.- NavigationContainer (preferentemente uno para toda la App)
+ * 2.- Contenedor de forma de navegación (Stack, Tab, Drawer) ejem = Stack
+ * 3.- Indicar las screens relacionadas a ese contenedor de navegacion
+ */
+//1.-
+import { NavigationContainer } from '@react-navigation/native';
+
+//2.-
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from './src/screens/Login';
+import Inicio from './src/screens/Inicio';
+
+//2.1.- Creamos el contenedo de la navegación
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	/** Este componente NO RETORNA NINGUNA SCREEN DIRECTAMENTE
+	 * Solo indicar la ruta de navegación de todos los componentes
+	 */
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName='Inicio'>
+				{/** Paso 3 */}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+				<Stack.Screen
+					name='Login'
+					component={Login}
+				/>
+
+				<Stack.Screen
+					name='Inicio'
+					component={Inicio}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
---
+++ src/screens/Inicio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Inicio = () => {
+	return (
+		<View
+			style={{
+				flex: 1,
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}
+		>
+			<Text>En Inicio</Text>
+		</View>
+	);
+};
+
+export default Inicio;
