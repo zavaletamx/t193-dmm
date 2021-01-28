3605d5a9b70a384fa596df5f5cc951b975a25261
ZavaletaMX <raul.zavaletazea@gmail.com>
2021-01-28 11:57:32 -0600

    Instalación de React Navigation 5, uso de Stack y ejemplo de navegación

--- src/screens/Inicio.js
+++ src/screens/Inicio.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 
-const Inicio = () => {
+/**
+ * Todos los componentes de React son capaces de compartir sus propiedades por medio
+ * de un parámetro en el incio de la funcion denominado props
+ *
+ * Dado que Inicio es parte de NavigationContainer tiene a su disposición
+ * todos los elementos de navegacion dentro de su objeto de propiedades
+ */
+const Inicio = (props) => {
 	return (
 		<View
 			style={{
@@ -11,6 +18,19 @@ const Inicio = () => {
 			}}
 		>
 			<Text>En Inicio</Text>
+			<Button
+				title='Al Login'
+				onPress={() => {
+					/**
+					 * Accedemos al objeto de popriedades, en la clave
+					 * navigation y accedemos a la funcion navigate
+					 *
+					 * Indicando el sobrenombre del componente podemos
+					 * enviar la navegación al siguiente elemento
+					 */
+					props.navigation.navigate('Login');
+				}}
+			/>
 		</View>
 	);
 };
