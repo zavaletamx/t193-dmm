4054eb64dd4382fc442656d7b797ce6adc52388d
ZavaletaMX <raul.zavaletazea@gmail.com>
2021-02-23 13:33:27 -0600

    Intro a Storage y uso de SnapShot

--- src/screens/private/Inicio.js
+++ src/screens/private/Inicio.js
@@ -1,8 +1,12 @@
 import { useFocusEffect } from '@react-navigation/core';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
+
+import firebase from './../../database/firebase';
 
 const Inicio = (props) => {
+	const [peliculas, setPeliculas] = React.useState([]);
+
 	/*
     Creamos un efecto que permita modificar el título 
     del headerNav
@@ -14,9 +18,51 @@ const Inicio = (props) => {
 		});
 	});
 
+	/**
+	 * Creamos un efecto que no este enganchado a ningún componente
+	 * (Sólo se eejcuta al inicializarse el screen)
+	 */
+	React.useEffect(() => {
+		/**
+		 * Un snapshot es una versión almacenada en cache de la BD
+		 * Es una copia local de información que está ligada a cualquier cambio
+		 * ya sea local o remoto
+		 *
+		 * Creamos un snapshot sobre una colección (consulta)
+		 */
+		firebase.db
+			.collection('peliculas')
+			.onSnapshot((querySnapshot) => {
+				const arrPelis = [];
+
+				/**
+				 * querSnapshot contiene una porpiedad llamada docs
+				 * docs contiene un arreglo con todos los documentos
+				 * de la colección
+				 *
+				 * Recorremos en su totalidad la propiedad docs
+				 */
+				querySnapshot.docs.map((doc) => {
+					//data contiene un wrapper de la info de cada
+					//documento
+					arrPelis.push({
+						peliculaId: doc.id,
+						...doc.data(),
+					});
+				});
+				setPeliculas(arrPelis);
+			});
+	}, []);
+
 	return (
-		<View>
-			<Text>Inicio.js</Text>
+		<View style={{ flex: 1 }}>
+			<FlatList
+				data={peliculas}
+				renderItem={(item) => (
+					<Text>{item.item.titulo}</Text>
+				)}
+				key={(item) => item.peliculaId}
+			/>
 		</View>
 	);
 };
